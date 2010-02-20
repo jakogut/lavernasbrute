8c4b8e6ea6d3aa3b59496e3a1f0c4b17cc4ff018
joseph.kogut <joseph.kogut@d899f61a-e3ff-11dd-ab33-bda5dbf46efd>
2010-02-20 23:16:30 +0000

    * Modified Laverna's Brute to use the dense hash map written by Google as part of the sparsehash library.

---
+++ branches/unstable/sparsehash/source/KeyGenerator.h
@@ -0,0 +1,129 @@
+// Part of Laverna's Brute
+
+#ifndef KEYGENERATOR_H_
+#define KEYGENERATOR_H_
+
+#include <vector>
+#include <cstring>
+#include <cmath>
+#include <limits.h>
+
+class keyGenerator
+{
+public:
+
+        keyGenerator(unsigned long long location, char* charset)
+                : charset(charset), location(location)
+        {
+                charsetLength = (unsigned int)strlen(charset);
+                integerToKey();
+        }
+
+        ~keyGenerator()
+        {
+        }
+
+        void incrementKey()
+        {
+                for(unsigned int place = 0; place < key.length(); place++)
+                {
+                        if(key[place] == charset[charsetLength - 1])
+                        {
+                                // Overflow, reset char at place
+                                key[place] = charset[0];
+                                keyIndices[place] = 0;
+
+                                if((key.length() - 1) < (place + 1))
+                                {
+                                        // Carry, no space, insert char
+                                        key.insert(key.begin(), charset[0]);
+                                        keyIndices.insert(keyIndices.begin(), 0);
+                                        break;
+                                }
+                                else
+                                {
+                                        continue;
+                                }
+                        }
+                        else
+                        {
+                                // Space available, increment char at place
+                                key[place] = charset[keyIndices[place] + 1];
+                                keyIndices[place] = keyIndices[place] + 1;
+                                break;
+                        }
+                }
+        }
+
+        inline std::string operator++()
+        {
+                incrementKey();
+                return getKey();
+        }
+
+        inline std::string operator++(int)
+        {
+                std::string retval = key;
+                incrementKey();
+
+                return retval;
+        }
+
+        void integerToKey()
+        {
+                unsigned long long num = location;
+
+                if(!num)
+                {
+                        key.insert(key.begin(), charset[0]);
+                        keyIndices.insert(keyIndices.begin(), 0);
+                }
+                else
+                {
+                        num++;
+
+                        while(num)
+                        {
+                                num--;
+                                unsigned long long remainder = num % charsetLength;
+                                num /= charsetLength;
+
+                                key.insert(key.end(), charset[remainder]);
+                                keyIndices.insert(keyIndices.end(), remainder);
+                        }
+                }
+        }
+
+        inline unsigned long long keyToInteger()
+        {
+                // TODO
+                return 0;
+        }
+
+        inline std::string getKey()
+        {
+                return key;
+        }
+
+        inline void getMultipleKeys(std::string* output, int num)
+        {
+                for(int i = 0; i < num; i++)
+                {
+                        output[i] = key;
+                        incrementKey();
+                }
+        }
+
+protected:
+
+        char* charset;
+
+        unsigned int charsetLength;
+
+        std::string key;
+        std::vector<unsigned int> keyIndices;
+
+        unsigned long long location;
+};
+
+#endif
