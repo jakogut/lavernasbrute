b1faf88e214d21c02368a93e3ac02e1f1cdcca58
joseph.kogut <joseph.kogut@d899f61a-e3ff-11dd-ab33-bda5dbf46efd>
2010-03-24 00:48:16 +0000

    * Improved commenting. * Removed a few unnecessary addition operations from the MD4 algorithm.

--- branches/unstable/newKeyGen/source/hashing/MD4_SSE2.h
+++ branches/unstable/newKeyGen/source/hashing/MD4_SSE2.h
@@ -6,16 +6,37 @@
 #include "MD4.h"
 #include "KeyGenerator.h"
 
+/* The rounds with "null" in the name omit the adding of the message section, 
+because if the hash is short enough to be computationally feasible to crack, 
+that element of the message will be zero. Since each ascii character is 8 bits,
+and each unicode character is 16 bits, this means that each 32-bit word that composes 
+the message can store either four ascii characters per word, or two unicode characters 
+per word. If it is computationally infeasible to attack the hash to any plaintext more 
+than 14 characters in length, that means we can assume that every element beyond the 4th
+for MD4, and 7th for NTLM are zeros.*/
+
 #define round1_sse2(set, wd_index, a, b, c, ntb_index, rotation) \
 	wd[set][wd_index] = _mm_add_epi32(wd[set][wd_index], _mm_add_epi32(_mm_or_si128(_mm_and_si128(wd[set][a], wd[set][b]), _mm_andnot_si128(wd[set][a], wd[set][c])), md4_buffer[set][ntb_index])), \
+	wd[set][wd_index] = ROTL_sse2(wd[set][wd_index], rotation)
+
+#define round1_null_sse2(set, wd_index, a, b, c, ntb_index, rotation) \
+	wd[set][wd_index] = _mm_add_epi32(wd[set][wd_index], _mm_or_si128(_mm_and_si128(wd[set][a], wd[set][b]), _mm_andnot_si128(wd[set][a], wd[set][c]))), \
 	wd[set][wd_index] = ROTL_sse2(wd[set][wd_index], rotation)
 
 #define round2_sse2(set, wd_index, a, b, c, ntb_index, rotation) \
 	wd[set][wd_index] = _mm_add_epi32(wd[set][wd_index], _mm_add_epi32(_mm_add_epi32(_mm_or_si128(_mm_and_si128(wd[set][a], _mm_or_si128(wd[set][b], wd[set][c])), _mm_and_si128(wd[set][b], wd[set][c])), md4_buffer[set][ntb_index]), SQRT_2)), \
 	wd[set][wd_index] = ROTL_sse2(wd[set][wd_index], rotation)
 
+#define round2_null_sse2(set, wd_index, a, b, c, ntb_index, rotation) \
+	wd[set][wd_index] = _mm_add_epi32(wd[set][wd_index], _mm_add_epi32(_mm_or_si128(_mm_and_si128(wd[set][a], _mm_or_si128(wd[set][b], wd[set][c])), _mm_and_si128(wd[set][b], wd[set][c])), SQRT_2)), \
+	wd[set][wd_index] = ROTL_sse2(wd[set][wd_index], rotation)
+
 #define round3_sse2(set, wd_index, a, b, c, ntb_index, rotation) \
 	wd[set][wd_index] = _mm_add_epi32(_mm_add_epi32(_mm_add_epi32(wd[set][wd_index], _mm_xor_si128(wd[set][a], _mm_xor_si128(wd[set][b], wd[set][c]))), md4_buffer[set][ntb_index]), SQRT_3), \
+	wd[set][wd_index] = ROTL_sse2(wd[set][wd_index], rotation)
+
+#define round3_null_sse2(set, wd_index, a, b, c, ntb_index, rotation) \
+	wd[set][wd_index] = _mm_add_epi32(_mm_add_epi32(wd[set][wd_index], _mm_xor_si128(wd[set][a], _mm_xor_si128(wd[set][b], wd[set][c]))), SQRT_3), \
 	wd[set][wd_index] = ROTL_sse2(wd[set][wd_index], rotation)
 
 #define ROTL_sse2(num, places) (_mm_or_si128(_mm_slli_epi32(num, places), _mm_srli_epi32(num, (32 - places))))
@@ -26,6 +47,7 @@ public:
 
 	MD4_SSE2()
 	{
+		// The square root of two and three, little endian
 		SQRT_2 = _mm_set1_epi32(0x5a827999);
 		SQRT_3 = _mm_set1_epi32(0x6ed9eba1);
 	}
@@ -48,7 +70,7 @@ public:
 
 	void getMultipleWeakNTLMHashes(std::string input[12], int64_pair output[12])
 	{
-		prepare_key_ntlm_sse2(input);
+		//prepare_key_ntlm_sse2(input);
 
 		initialize_words_sse2();
 
@@ -158,12 +180,12 @@ protected:
 		round1_sse2(0, 3, 0, 1, 2, 1, 7);
 		round1_sse2(1, 3, 0, 1, 2, 1, 7);
 		round1_sse2(2, 3, 0, 1, 2, 1, 7);
-		round1_sse2(0, 2, 3, 0, 1, 2, 11);
-		round1_sse2(1, 2, 3, 0, 1, 2, 11);
-		round1_sse2(2, 2, 3, 0, 1, 2, 11);
-		round1_sse2(0, 1, 2, 3, 0, 3, 19);
-		round1_sse2(1, 1, 2, 3, 0, 3, 19);
-		round1_sse2(2, 1, 2, 3, 0, 3, 19);
+		round1_null_sse2(0, 2, 3, 0, 1, 2, 11);
+		round1_null_sse2(1, 2, 3, 0, 1, 2, 11);
+		round1_null_sse2(2, 2, 3, 0, 1, 2, 11);
+		round1_null_sse2(0, 1, 2, 3, 0, 3, 19);
+		round1_null_sse2(1, 1, 2, 3, 0, 3, 19);
+		round1_null_sse2(2, 1, 2, 3, 0, 3, 19);
 		
 
 		round1_sse2(0, 0, 1, 2, 3, 4, 3);
@@ -172,12 +194,12 @@ protected:
 		round1_sse2(0, 3, 0, 1, 2, 5, 7);
 		round1_sse2(1, 3, 0, 1, 2, 5, 7);
 		round1_sse2(2, 3, 0, 1, 2, 5, 7);
-		round1_sse2(0, 2, 3, 0, 1, 6, 11);
-		round1_sse2(1, 2, 3, 0, 1, 6, 11);
-		round1_sse2(2, 2, 3, 0, 1, 6, 11);
-		round1_sse2(0, 1, 2, 3, 0, 7, 19);
-		round1_sse2(1, 1, 2, 3, 0, 7, 19);
-		round1_sse2(2, 1, 2, 3, 0, 7, 19);
+		round1_null_sse2(0, 2, 3, 0, 1, 6, 11);
+		round1_null_sse2(1, 2, 3, 0, 1, 6, 11);
+		round1_null_sse2(2, 2, 3, 0, 1, 6, 11);
+		round1_null_sse2(0, 1, 2, 3, 0, 7, 19);
+		round1_null_sse2(1, 1, 2, 3, 0, 7, 19);
+		round1_null_sse2(2, 1, 2, 3, 0, 7, 19);
 		
 
 		round1_sse2(0, 0, 1, 2, 3, 8, 3);
@@ -186,12 +208,12 @@ protected:
 		round1_sse2(0, 3, 0, 1, 2, 9, 7);
 		round1_sse2(1, 3, 0, 1, 2, 9, 7);
 		round1_sse2(2, 3, 0, 1, 2, 9, 7);
-		round1_sse2(0, 2, 3, 0, 1, 10, 11);
-		round1_sse2(1, 2, 3, 0, 1, 10, 11);
-		round1_sse2(2, 2, 3, 0, 1, 10, 11);
-		round1_sse2(0, 1, 2, 3, 0, 11, 19);
-		round1_sse2(1, 1, 2, 3, 0, 11, 19);
-		round1_sse2(2, 1, 2, 3, 0, 11, 19);
+		round1_null_sse2(0, 2, 3, 0, 1, 10, 11);
+		round1_null_sse2(1, 2, 3, 0, 1, 10, 11);
+		round1_null_sse2(2, 2, 3, 0, 1, 10, 11);
+		round1_null_sse2(0, 1, 2, 3, 0, 11, 19);
+		round1_null_sse2(1, 1, 2, 3, 0, 11, 19);
+		round1_null_sse2(2, 1, 2, 3, 0, 11, 19);
 		
 
 		round1_sse2(0, 0, 1, 2, 3, 12, 3);
@@ -200,12 +222,12 @@ protected:
 		round1_sse2(0, 3, 0, 1, 2, 13, 7);
 		round1_sse2(1, 3, 0, 1, 2, 13, 7);
 		round1_sse2(2, 3, 0, 1, 2, 13, 7);
-		round1_sse2(0, 2, 3, 0, 1, 14, 11);
-		round1_sse2(1, 2, 3, 0, 1, 14, 11);
-		round1_sse2(2, 2, 3, 0, 1, 14, 11);
-		round1_sse2(0, 1, 2, 3, 0, 15, 19);
-		round1_sse2(1, 1, 2, 3, 0, 15, 19);
-		round1_sse2(2, 1, 2, 3, 0, 15, 19);
+		round1_null_sse2(0, 2, 3, 0, 1, 14, 11);
+		round1_null_sse2(1, 2, 3, 0, 1, 14, 11);
+		round1_null_sse2(2, 2, 3, 0, 1, 14, 11);
+		round1_null_sse2(0, 1, 2, 3, 0, 15, 19);
+		round1_null_sse2(1, 1, 2, 3, 0, 15, 19);
+		round1_null_sse2(2, 1, 2, 3, 0, 15, 19);
 		
 
 		// Round 2 // ---
@@ -216,12 +238,12 @@ protected:
 		round2_sse2(0, 3, 0, 1, 2, 4, 5);
 		round2_sse2(1, 3, 0, 1, 2, 4, 5);
 		round2_sse2(2, 3, 0, 1, 2, 4, 5);
-		round2_sse2(0, 2, 3, 0, 1, 8, 9);
-		round2_sse2(1, 2, 3, 0, 1, 8, 9);
-		round2_sse2(2, 2, 3, 0, 1, 8, 9);
-		round2_sse2(0, 1, 2, 3, 0, 12, 13);
-		round2_sse2(1, 1, 2, 3, 0, 12, 13);
-		round2_sse2(2, 1, 2, 3, 0, 12, 13);
+		round2_null_sse2(0, 2, 3, 0, 1, 8, 9);
+		round2_null_sse2(1, 2, 3, 0, 1, 8, 9);
+		round2_null_sse2(2, 2, 3, 0, 1, 8, 9);
+		round2_null_sse2(0, 1, 2, 3, 0, 12, 13);
+		round2_null_sse2(1, 1, 2, 3, 0, 12, 13);
+		round2_null_sse2(2, 1, 2, 3, 0, 12, 13);
 		
 
 		round2_sse2(0, 0, 1, 2, 3, 1, 3);
@@ -230,12 +252,12 @@ protected:
 		round2_sse2(0, 3, 0, 1, 2, 5, 5);
 		round2_sse2(1, 3, 0, 1, 2, 5, 5);
 		round2_sse2(2, 3, 0, 1, 2, 5, 5);
-		round2_sse2(0, 2, 3, 0, 1, 9, 9);
-		round2_sse2(1, 2, 3, 0, 1, 9, 9);
-		round2_sse2(2, 2, 3, 0, 1, 9, 9);
-		round2_sse2(0, 1, 2, 3, 0, 13, 13);
-		round2_sse2(1, 1, 2, 3, 0, 13, 13);
-		round2_sse2(2, 1, 2, 3, 0, 13, 13);
+		round2_null_sse2(0, 2, 3, 0, 1, 9, 9);
+		round2_null_sse2(1, 2, 3, 0, 1, 9, 9);
+		round2_null_sse2(2, 2, 3, 0, 1, 9, 9);
+		round2_null_sse2(0, 1, 2, 3, 0, 13, 13);
+		round2_null_sse2(1, 1, 2, 3, 0, 13, 13);
+		round2_null_sse2(2, 1, 2, 3, 0, 13, 13);
 		
 
 		round2_sse2(0, 0, 1, 2, 3, 2, 3);
@@ -244,12 +266,12 @@ protected:
 		round2_sse2(0, 3, 0, 1, 2, 6, 5);
 		round2_sse2(1, 3, 0, 1, 2, 6, 5);
 		round2_sse2(2, 3, 0, 1, 2, 6, 5);
-		round2_sse2(0, 2, 3, 0, 1, 10, 9);
-		round2_sse2(1, 2, 3, 0, 1, 10, 9);
-		round2_sse2(2, 2, 3, 0, 1, 10, 9);
-		round2_sse2(0, 1, 2, 3, 0, 14, 13);
-		round2_sse2(1, 1, 2, 3, 0, 14, 13);
-		round2_sse2(2, 1, 2, 3, 0, 14, 13);
+		round2_null_sse2(0, 2, 3, 0, 1, 10, 9);
+		round2_null_sse2(1, 2, 3, 0, 1, 10, 9);
+		round2_null_sse2(2, 2, 3, 0, 1, 10, 9);
+		round2_null_sse2(0, 1, 2, 3, 0, 14, 13);
+		round2_null_sse2(1, 1, 2, 3, 0, 14, 13);
+		round2_null_sse2(2, 1, 2, 3, 0, 14, 13);
 		
 
 		round2_sse2(0, 0, 1, 2, 3, 3, 3);
@@ -258,12 +280,12 @@ protected:
 		round2_sse2(0, 3, 0, 1, 2, 7, 5);
 		round2_sse2(1, 3, 0, 1, 2, 7, 5);
 		round2_sse2(2, 3, 0, 1, 2, 7, 5);
-		round2_sse2(0, 2, 3, 0, 1, 11, 9);
-		round2_sse2(1, 2, 3, 0, 1, 11, 9);
-		round2_sse2(2, 2, 3, 0, 1, 11, 9);
-		round2_sse2(0, 1, 2, 3, 0, 15, 13);
-		round2_sse2(1, 1, 2, 3, 0, 15, 13);
-		round2_sse2(2, 1, 2, 3, 0, 15, 13);
+		round2_null_sse2(0, 2, 3, 0, 1, 11, 9);
+		round2_null_sse2(1, 2, 3, 0, 1, 11, 9);
+		round2_null_sse2(2, 2, 3, 0, 1, 11, 9);
+		round2_null_sse2(0, 1, 2, 3, 0, 15, 13);
+		round2_null_sse2(1, 1, 2, 3, 0, 15, 13);
+		round2_null_sse2(2, 1, 2, 3, 0, 15, 13);
 		
 
 		// Round 3 // --
@@ -271,57 +293,56 @@ protected:
 		round3_sse2(0, 0, 3, 2, 1, 0, 3);
 		round3_sse2(1, 0, 3, 2, 1, 0, 3);
 		round3_sse2(2, 0, 3, 2, 1, 0, 3);
-		round3_sse2(0, 3, 2, 1, 0, 8, 9);
-		round3_sse2(1, 3, 2, 1, 0, 8, 9);
-		round3_sse2(2, 3, 2, 1, 0, 8, 9);
-		round3_sse2(0, 2, 1, 0, 3, 4, 11);
-		round3_sse2(1, 2, 1, 0, 3, 4, 11);
-		round3_sse2(2, 2, 1, 0, 3, 4, 11);
-		round3_sse2(0, 1, 0, 3, 2, 12, 15);
-		round3_sse2(1, 1, 0, 3, 2, 12, 15);
-		round3_sse2(2, 1, 0, 3, 2, 12, 15);
+		round3_null_sse2(0, 3, 2, 1, 0, 8, 9);
+		round3_null_sse2(1, 3, 2, 1, 0, 8, 9);
+		round3_null_sse2(2, 3, 2, 1, 0, 8, 9);
+		round3_null_sse2(0, 2, 1, 0, 3, 4, 11);
+		round3_null_sse2(1, 2, 1, 0, 3, 4, 11);
+		round3_null_sse2(2, 2, 1, 0, 3, 4, 11);
+		round3_null_sse2(0, 1, 0, 3, 2, 12, 15);
+		round3_null_sse2(1, 1, 0, 3, 2, 12, 15);
+		round3_null_sse2(2, 1, 0, 3, 2, 12, 15);
 		
 
 		round3_sse2(0, 0, 3, 2, 1, 2, 3);
 		round3_sse2(1, 0, 3, 2, 1, 2, 3);
 		round3_sse2(2, 0, 3, 2, 1, 2, 3);
-		round3_sse2(0, 3, 2, 1, 0, 10, 9);
-		round3_sse2(1, 3, 2, 1, 0, 10, 9);
-		round3_sse2(2, 3, 2, 1, 0, 10, 9);
-		round3_sse2(0, 2, 1, 0, 3, 6, 11);
-		round3_sse2(1, 2, 1, 0, 3, 6, 11);
-		round3_sse2(2, 2, 1, 0, 3, 6, 11);
-		round3_sse2(0, 1, 0, 3, 2, 14, 15);
-		round3_sse2(1, 1, 0, 3, 2, 14, 15);
-		round3_sse2(2, 1, 0, 3, 2, 14, 15);
+		round3_null_sse2(0, 3, 2, 1, 0, 10, 9);
+		round3_null_sse2(1, 3, 2, 1, 0, 10, 9);
+		round3_null_sse2(2, 3, 2, 1, 0, 10, 9);
+		round3_null_sse2(0, 2, 1, 0, 3, 6, 11);
+		round3_null_sse2(2, 2, 1, 0, 3, 6, 11);
+		round3_null_sse2(0, 1, 0, 3, 2, 14, 15);
+		round3_null_sse2(1, 1, 0, 3, 2, 14, 15);
+		round3_null_sse2(2, 1, 0, 3, 2, 14, 15);
 		
 
 		round3_sse2(0, 0, 3, 2, 1, 1, 3);
 		round3_sse2(1, 0, 3, 2, 1, 1, 3);
 		round3_sse2(2, 0, 3, 2, 1, 1, 3);
-		round3_sse2(0, 3, 2, 1, 0, 9, 9);
-		round3_sse2(1, 3, 2, 1, 0, 9, 9);
-		round3_sse2(2, 3, 2, 1, 0, 9, 9);
-		round3_sse2(0, 2, 1, 0, 3, 5, 11);
-		round3_sse2(1, 2, 1, 0, 3, 5, 11);
-		round3_sse2(2, 2, 1, 0, 3, 5, 11);
-		round3_sse2(0, 1, 0, 3, 2, 13, 15);
-		round3_sse2(1, 1, 0, 3, 2, 13, 15);
-		round3_sse2(2, 1, 0, 3, 2, 13, 15);
+		round3_null_sse2(0, 3, 2, 1, 0, 9, 9);
+		round3_null_sse2(1, 3, 2, 1, 0, 9, 9);
+		round3_null_sse2(2, 3, 2, 1, 0, 9, 9);
+		round3_null_sse2(0, 2, 1, 0, 3, 5, 11);
+		round3_null_sse2(1, 2, 1, 0, 3, 5, 11);
+		round3_null_sse2(2, 2, 1, 0, 3, 5, 11);
+		round3_null_sse2(0, 1, 0, 3, 2, 13, 15);
+		round3_null_sse2(1, 1, 0, 3, 2, 13, 15);
+		round3_null_sse2(2, 1, 0, 3, 2, 13, 15);
 		
 
 		round3_sse2(0, 0, 3, 2, 1, 3, 3);
 		round3_sse2(1, 0, 3, 2, 1, 3, 3);
 		round3_sse2(2, 0, 3, 2, 1, 3, 3);
-		round3_sse2(0, 3, 2, 1, 0, 11, 9);
-		round3_sse2(1, 3, 2, 1, 0, 11, 9);
-		round3_sse2(2, 3, 2, 1, 0, 11, 9);
-		round3_sse2(0, 2, 1, 0, 3, 7, 11);
-		round3_sse2(1, 2, 1, 0, 3, 7, 11);
-		round3_sse2(2, 2, 1, 0, 3, 7, 11);
-		round3_sse2(0, 1, 0, 3, 2, 15, 15);
-		round3_sse2(1, 1, 0, 3, 2, 15, 15);
-		round3_sse2(2, 1, 0, 3, 2, 15, 15);
+		round3_null_sse2(0, 3, 2, 1, 0, 11, 9);
+		round3_null_sse2(1, 3, 2, 1, 0, 11, 9);
+		round3_null_sse2(2, 3, 2, 1, 0, 11, 9);
+		round3_null_sse2(0, 2, 1, 0, 3, 7, 11);
+		round3_null_sse2(1, 2, 1, 0, 3, 7, 11);
+		round3_null_sse2(2, 2, 1, 0, 3, 7, 11);
+		round3_null_sse2(0, 1, 0, 3, 2, 15, 15);
+		round3_null_sse2(1, 1, 0, 3, 2, 15, 15);
+		round3_null_sse2(2, 1, 0, 3, 2, 15, 15);
 		
 
 		for(int i = 0; i < 3; i++)
