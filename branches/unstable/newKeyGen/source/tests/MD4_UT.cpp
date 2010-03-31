1ba84c7e144abad1177aed69330a6bdf76ec9398
joseph.kogut <joseph.kogut@d899f61a-e3ff-11dd-ab33-bda5dbf46efd>
2010-03-31 18:59:44 +0000

    * Updated tests * Fixed compilation errors

--- branches/unstable/newKeyGen/source/tests/MD4_UT.cpp
+++ branches/unstable/newKeyGen/source/tests/MD4_UT.cpp
@@ -7,31 +7,31 @@ using namespace std;
 
 int main()
 {
-    MD4 md4;
+	MD4 md4;
 
-    std::string input = "testString";
-    
-    std::string correctFullHash = "d25cd189889817578d36756cd6801bc5";
-    int64_pair correctWeakHash = md4.weakenHash(correctFullHash);
-    
-    // Test for vanilla C full MD4 algorithm
-    if(md4.getMD4Hash(input) == correctFullHash)
-    {
-        cout << "Vanilla MD4 algorithm passed full hashing test." << endl;
-    }
-    else
-    {
-        cout << "Vanilla MD4 algorithm FAILED full hashing test." << endl;
-    }
-    
-    // Test for vanilla C partial MD4 algorithm
-    if(md4.getWeakMD4Hash(input) == correctWeakHash)
-    {
-        cout << "Vanilla MD4 algorithm passed partial hashing test." << endl;
-    }
-    else
-    {
-        cout << "Vanilla MD4 algorithm FAILED partial hashing test." << endl;
-    }
+	const char* input = "testString";
+
+	const char* correctFullHash = "d25cd189889817578d36756cd6801bc5";
+	int64_pair correctWeakHash = md4.weakenHash(correctFullHash);
+
+	// Test for vanilla C full MD4 algorithm
+	if(md4.getHash_MD4(input) == correctFullHash)
+	{
+		cout << "Vanilla MD4 algorithm passed full hashing test." << endl;
+	}
+	else
+	{
+		cout << "Vanilla MD4 algorithm FAILED full hashing test." << endl;
+	}
+
+	// Test for vanilla C partial MD4 algorithm
+	if(md4.getWeakHash_MD4(input) == correctWeakHash)
+	{
+		cout << "Vanilla MD4 algorithm passed partial hashing test." << endl;
+	}
+	else
+	{
+		cout << "Vanilla MD4 algorithm FAILED partial hashing test." << endl;
+	}
 }
 
--- branches/unstable/newKeyGen/source/tests/NTLM_UT.cpp
+++ branches/unstable/newKeyGen/source/tests/NTLM_UT.cpp
@@ -7,31 +7,31 @@ using namespace std;
 
 int main()
 {
-    MD4 md4;
-    
-    std::string input = "testString";
-    
-    std::string correctFullHash = "7c404e7a355530e496d6f5612a1ed54e";
-    int64_pair correctWeakHash = md4.weakenHash(correctFullHash);
-    
-    // Test for vanilla C full NTLM algorithm
-    if(md4.getNTLMHash(input) == correctFullHash)
-    {
-        cout << "Vanilla NTLM algorithm passed full hashing test." << endl;
-    }
-    else
-    {
-        cout << "Vanilla NTLM algorithm FAILED full hashing test." << endl;
-    }
-    
-    // Test for vanilla C partial NTLM algorithm
-    if(md4.getWeakNTLMHash(input) == correctWeakHash)
-    {
-        cout << "Vanilla NTLM algorithm passed partial hashing test." << endl;
-    }
-    else
-    {
-        cout << "Vanilla NTLM algorithm FAILED partial hashing test." << endl;
-    }
+	MD4 md4;
+
+	const char* input = "testString";
+
+	const char* correctFullHash = "7c404e7a355530e496d6f5612a1ed54e";
+	int64_pair correctWeakHash = md4.weakenHash(correctFullHash);
+
+	// Test for vanilla C full NTLM algorithm
+	if(md4.getHash_NTLM(input) == correctFullHash)
+	{
+		cout << "Vanilla NTLM algorithm passed full hashing test." << endl;
+	}
+	else
+	{
+		cout << "Vanilla NTLM algorithm FAILED full hashing test." << endl;
+	}
+
+	// Test for vanilla C partial NTLM algorithm
+	if(md4.getWeakHash_NTLM(input) == correctWeakHash)
+	{
+		cout << "Vanilla NTLM algorithm passed partial hashing test." << endl;
+	}
+	else
+	{
+		cout << "Vanilla NTLM algorithm FAILED partial hashing test." << endl;
+	}
 }
 
