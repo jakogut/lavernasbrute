9718ff4d9297137943b27b94d70edb021e8de19c
joseph.kogut <joseph.kogut@d899f61a-e3ff-11dd-ab33-bda5dbf46efd>
2010-04-04 20:35:31 +0000

    * Added tests to trunk.

---
+++ trunk/source/tests/NTLM_SSE2_UT.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+#include "MD4_SSE2.h"
+
+using namespace std;
+
+int main()
+{
+	MD4_SSE2 md4_md;
+	MD4 md4;
+
+	char* test = "testString";
+
+	char* input[12];
+	int64_pair output[12];
+
+	for(int i = 0; i < 12; i++)
+	{
+		input[i] = new char[16];
+		strcpy(input[i], test);
+	}
+
+	md4_md.getWeakHashes_NTLM(input, output);
+
+	int64_pair vanillaResult = md4.getWeakHash_NTLM(test);
+
+	bool passed = true;
+	vector<int> failedHashes;
+
+	for(int i = 0; i < 12; i++)
+	{
+		if(output[i] != vanillaResult)
+		{
+			passed = false;
+			failedHashes.push_back(i);
+		}
+	}
+
+	if(passed)
+	{
+	cout << "SSE2 accelerated NTLM algorithm passed the test." << endl;
+	}
+	else
+	{
+		cout << "SSE2 accelerated NTLM algorithm FAILED the test." << endl;
+
+		for(unsigned int i = 0; i < failedHashes.size(); i++)
+			cout << "\nString " << failedHashes[i] << " was not hashed properly." << endl;
+	}
+
+	for(int i = 0; i < 12; i++) delete [] input[i];
+
+	return 0;
+}
+
