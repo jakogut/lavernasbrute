1d57881fbc5ba866fe3c6da317d47784a21839a4
joseph.kogut <joseph.kogut@d899f61a-e3ff-11dd-ab33-bda5dbf46efd>
2010-11-05 07:45:50 +0000

    * Created CUDA processing path.

---
+++ source/CUDAPath.cuh
@@ -0,0 +1,42 @@
+// Part of Laverna's Brute
+
+#ifndef CUDA_PATH_H_
+#define CUDA_PATH_H_
+
+#include "ProcessingPath.h"
+#include "MasterThread.h"
+
+#include "hashing/MD4_CUDA.cuh"
+
+class CUDAPath
+{
+public:
+
+	CUDAPath(int id);
+	~CUDAPath();
+
+	void operator()();
+	void searchKeyspace();
+
+	unsigned int getThreadID();
+
+	unsigned long long getKeyspaceEnd();
+	unsigned long long getKeyspaceBegin();
+	unsigned long long getKeyspaceLocation();
+
+	void moveKeyspaceEnd(unsigned long long input);
+	void moveKeyspaceBegin(unsigned long long input);
+	void moveKeyspaceLocation(unsigned long long input);
+
+protected:
+
+	int id;
+
+	std::vector<hashContext>::iterator targetIterator;
+
+	MD4_CUDA cu_md4;
+
+	unsigned long long keyspaceEnd, keyspaceBegin, keyspaceLocation;
+
+	hashContext* ctx;
+}
